26ed524d06fb8c4e0e9f222e5e8a934d9d323dc6
GrindFreak <omgmycanonminion@gmail.com>
2022-12-18 03:06:33 +0200

    Initial Commit

--- qtile/config.py
+++ qtile/config.py
@@ -9,8 +9,6 @@
 
 ######################################################################
 
-#test
-
 from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
